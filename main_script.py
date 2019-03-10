05e57dc906eec59155fbea6492f5f0ffc6518145
aakashchandhoke <aakash.chandhoke24@gmail.com>
2019-03-10 09:50:07 +0530

    Renaming of files and adding comments

--- main_script.py
+++ main_script.py
@@ -1,3 +1,6 @@
+############################                 Python script that is capable running SUMO with the                  #########################
+############################                                required scenarios                                    #########################
+
 import os, sys
 import time
 import sys
@@ -30,14 +33,19 @@ try:
     from sumolib.net import Roundabout
 
 except ImportError:
-    sys.exit("please declare environment variable 'SUMO_HOME' as the root directory of your sumo installation (it should contain folders 'bin',tools' and 'docs')")
+    sys.exit("please declare environment variable 'SUMO_HOME' as the root directory \
+        of your sumo installation (it should contain folders 'bin',tools' and 'docs')")
+graph = sumolib.net.readNet('Dijkstra.net.xml')
 
-graph = sumolib.net.readNet('test.net.xml')
+######################################################################################################################################
+# Class used in finding the shortest rout path from Source to Destination
+######################################################################################################################################
+
 class priorityDictionary(dict):
     def __init__(self):
-        '''Initialize priorityDictionary by creating binary heap
-            of pairs (value,key).  Note that changing or removing a dict entry 
+        '''Initialize priorityDictionary by creating binary heap of pairs (value,key). Note that changing or removing a dict entry 
             will not remove the old pair from the heap until it is found by smallest() or until the heap is rebuilt.'''
+
         self.__heap = []
         dict.__init__(self)
 
@@ -99,6 +107,10 @@ grows
         for key in other.keys():
             self[key] = other[key]
 
+######################################################################################################################################
+# Normal Dijkstra's Algorithm that takes the starting source edge and ending Destination edge
+######################################################################################################################################
+
 def Dijkstra(graph, start, end=None):
     D = {}    # dictionary of final distances
     P = {}    # dictionary of predecessors
@@ -115,6 +127,12 @@ def Dijkstra(graph, start, end=None):
                 P[edge] = vertex
     return (D,P)
 
+######################################################################################################################################
+# Dijkstra's Algorithm that takes into account the congestion of the road.
+# 0 means max Congestion
+# 1 means min COngestion
+######################################################################################################################################
+
 def Dijkstra_with_congestion(graph, start, end=None):
     ## congestion at 0 is max and congestion at 1 is min
     D = {}    # dictionary of final distances
@@ -122,7 +140,7 @@ def Dijkstra_with_congestion(graph, start, end=None):
     Q = priorityDictionary()    # estimated distances of non-final vertices
     Q[start] = 0
     edge = graph.getEdges()
-    congestion = getCongestion('test.taz.xml')
+    congestion = getCongestion('Dijkstra.taz.xml')
     for vertex in Q:
         D[vertex] = Q[vertex]
         if vertex == end: break
@@ -138,14 +156,21 @@ def Dijkstra_with_congestion(graph, start, end=None):
                     P[edge] = vertex
     return (D,P)
 
+######################################################################################################################################
+# Dijkstra's Algorithm that takes into account the overhead of the road.
+# Overhead means construction, width of the road, congestion param, etc.
+# 0 means max Overhead
+# 1 means min Overhead
+######################################################################################################################################
+
 def Dijkstra_with_overheads(graph, start, end=None):
-    ## congestion at 0 is max and congestion at 1 is min
+    ## 0 means max overhead and 1 means minimum overhhead
     D = {}    # dictionary of final distances
     P = {}    # dictionary of predecessors
     Q = priorityDictionary()    # estimated distances of non-final vertices
     Q[start] = 0
     edge = graph.getEdges()
-    overheads = getOverheads('test.taz.xml')
+    overheads = getOverheads('Dijkstra.taz.xml')
     for vertex in Q:
         D[vertex] = Q[vertex]
         if vertex == end: break
@@ -161,31 +186,41 @@ def Dijkstra_with_overheads(graph, start, end=None):
                     P[edge] = vertex
     return (D,P)
 
+######################################################################################################################################
+# Generation of route file from the output of the algorithms
+# Red vehiles are the congested vehicles
+# Blue vehicles are the vehicles that the ambulance will take
+######################################################################################################################################
+
 def generate_routefile(route):
-    with open("dijk1.rou.xml", "w") as routes:
+    with open("Dijkstra.rou.xml", "w") as routes:
         routes.write('<routes> \n')
 
-        #Congestion from Left to Right
+        # Congestion from Left to Right
         for j in range(7):
             routes.write('<vType id=\"vehicleLEFTRIGHT'+str(j)+'\" accel=\"0.8\" decel=\"4.5\" sigma=\"0.5\" length=\"5\" minGap=\"2.5\" maxSpeed=\"16.67\" guiShape=\"passenger\" /> \n')
             routes.write('<vehicle id =\"1223'+str(j)+'\" depart=\"'+str(0.5+j)+'\" departLane=\"free\" departSpeed=\"max\"> \n')
             routes.write('<route id=\"'+str(j)+'\" color="red" edges="422766356#0 422766356#1 422766356#2" /> \n')
             routes.write('</vehicle> \n')
 
-        #Congestion from Top to Down
+        # Congestion from Top to Down
         for k in range(50):
             routes.write('<vType id=\"vehicleUPDown'+str(k)+'\" accel=\"0.8\" decel=\"4.5\" sigma=\"0.5\" length=\"5\" minGap=\"2.5\" maxSpeed=\"16.67\" guiShape=\"moped\" /> \n')
             routes.write('<vehicle id =\"23456'+str(k)+'\" depart=\"'+str(0.5+k)+'\" departLane=\"free\" departSpeed=\"max\"> \n')
             routes.write('<route id=\"'+str(k)+'\" color="red" edges="422766370#0 422766370#1 422766370#2 422766370#3 422766370#4 422766370#5 422766370#6 422766366#0 422766366#1 422766366#2 422766366#3" /> \n')
             routes.write('</vehicle> \n')
 
-        # ## Creating 100 vehicles for simulation
+        # Creating 100 vehicles of ambulance type to show the path covered by the vehicle for simulation
         for i in range(100):
             routes.write('<vType id=\"vehicle'+str(i)+'\" accel=\"0.8\" decel=\"4.5\" sigma=\"0.5\" length=\"5\" minGap=\"2.5\" maxSpeed=\"16.67\" guiShape=\"delivery\" /> \n')
             routes.write('<vehicle id =\"'+str(i)+'\" depart=\"'+str(+i)+'\" departLane=\"free\" departSpeed=\"max\"> \n')
             routes.write('<route id=\"'+str(i)+'\" color="blue" edges=\"'+route+'\" /> \n')
             routes.write('</vehicle> \n')
         routes.write('</routes>')
+
+######################################################################################################################################
+# Function to get the overhead from the TAZ file
+######################################################################################################################################
 
 def getOverheads(filename):
     congestion = dict()
@@ -199,6 +234,10 @@ def getOverheads(filename):
     congestion = sorted(congestion.items(), key=operator.itemgetter(0))
     return congestion
 
+######################################################################################################################################
+# Function to get the congestion from the TAZ file
+######################################################################################################################################
+
 def getCongestion(filename):
     congestion = dict()
     tree = ET.parse(filename)
@@ -211,13 +250,14 @@ def getCongestion(filename):
     congestion = sorted(congestion.items(), key=operator.itemgetter(0))
     return congestion
 
+######################################################################################################################################
+# Function to find the shortest path from the starting edge to the ending edge.
+# Finding a single shortest path from the given start vertex to the given end vertex. 
+# The input has the same conventions as Dijkstra().
+# The output is a list of the vertices in order along the shortest path.
+######################################################################################################################################
+
 def shortestPath(graph, start, end, arg):
-    """
-    Find a single shortest path from the given start vertex to the given
-end vertex.
-    The input has the same conventions as Dijkstra().
-    The output is a list of the vertices in order along the shortest path.
-    """
     start = graph.getEdge(start)
     end = graph.getEdge(end)
     if arg == 0:
@@ -246,6 +286,23 @@ end vertex.
     Path.reverse()
     return Path
 
+######################################################################################################################################
+# Function to take the command line arguments of a certain type
+######################################################################################################################################
+
+def initOptions():
+    argParser = argparse.ArgumentParser()
+    ## added param to give the command as of what type of Algorithm we need to choose
+    argParser.add_argument("-r", "--routing_algorithm", default="dijkstra", type=str, help="select the routing algorithm")
+    ## added param to give SUMO the command to run as a command line version or GUI version
+    argParser.add_argument("--nogui", action="store_true", default=False, help="run the commandline version of sumo")
+    args = argParser.parse_args()
+    return args
+
+######################################################################################################################################
+# The first calling function in the program
+######################################################################################################################################
+
 def main(route):
     edges = [edge.getID() for edge in route]
     string = ""
@@ -253,29 +310,31 @@ def main(route):
         string += unicodedata.normalize('NFKD', data).encode('ascii','ignore') + " "
     generate_routefile(string)
 
-def initOptions():
-    argParser = argparse.ArgumentParser()
-    argParser.add_argument("-r", "--routing_algorithm", default="dijkstra", type=str, help="select the routing algorithm")
-    argParser.add_argument("--nogui", action="store_true", default=False, help="run the commandline version of sumo")
-    args = argParser.parse_args()
-    return args
+######################################################################################################################################
+# This is the main entry point of this script.
+######################################################################################################################################
 
-# this is the main entry point of this script
 if __name__ == "__main__":
     options = initOptions()
     # this script has been called from the command line. It will start sumo as a
     # server, then connect and run
     if options.nogui:
-       sumoBinary = checkBinary('sumo')
+       sumoBinary = checkBinary('sumo') ## Command Line version of SUMO
     else:
-       sumoBinary = checkBinary('sumo-gui')
+       sumoBinary = checkBinary('sumo-gui') ## GUI version of SUMO
 
-    if options.routing_algorithm == "dijkstra-with-congestion":
-        route = shortestPath(graph, '-422766323#3', '-193223413#2', 1)
-    elif options.routing_algorithm == "dijkstra":
+    # 0 means default dijkstras algorithm
+    # 1 means dijkstras algorithm taking into account the congestion of the road
+    # 2 means dijkstras algorithm taking into account the overheads (means construction, width of the road, congestion param, etc) 
+    #         while driving on the road
+
+    if options.routing_algorithm == "dijkstra":
         route = shortestPath(graph, '-422766323#3', '-193223413#2', 0)
+    elif options.routing_algorithm == "dijkstra-with-congestion":
+        route = shortestPath(graph, '-422766323#3', '-193223413#2', 1)
     else:
         route = shortestPath(graph, '-422766323#3', '-193223413#2', 2)
+
     # this is the normal way of using traci. sumo is started as a
     # subprocess and then the python script connects and runs
     sumoProcess = subprocess.Popen([sumoBinary, "-c", "dijkstra.sumocfg"])
